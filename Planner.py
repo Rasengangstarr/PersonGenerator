4b9f4d93300c3a8dd195fa1d2096a9c628847227
Rasengangstarr <davidouthere@msn.com>
2020-04-09 17:49:53 +0100

    implemented framework for GOAD

--- Planner.py
+++ Planner.py
@@ -1,13 +1,102 @@
 from State import World
 from Plant import Plant
 
-class Goal():
-    def __init__(self):
-        self.prereqs =	{
-            "agentHasPlantTargetted": false,
-            
-            }
+# agentKnowsAboutPlant => agentTargetsPlant => agentHasPlantTargetted
+# agentHasPlantTargetted => agentMovesToTargetPlant => agentAtTargetPlant || agentTargetGone
+# agentTargetGone => RESET
+# agentAtTargetPlant => agentDestroysTargetPlant => agentKnowsAboutFood || RESET
+# agentKnowsAboutFood => agentTargetsFood => AgentHasFoodTargetted
+# agentHasFoodTargetted => agentMovesToTargetFood => agentAtTargetFood || agentTargetGone
+# agentAtTargetFood => agentPicksUpFood => agentHasFood
+# agentHasFood => agentEatsFood => [AGENTHUNGERDECREASED]
+
+actionPaths = [
+    "agentKnowsAboutPlant=>agentTargetsPlant=>agentHasPlantTargetted",
+    "agentHasPlantTargetted=>agentMovesToTargetPlant=>agentAtTargetPlant",
+    "agentTargetGone=>RESET",
+    "agentAtTargetPlant=>agentDestroysTargetPlant=>agentKnowsAboutFood",
+    "agentKnowsAboutFood=>agentTargetsFood=>agentHasFoodTargetted",
+    "agentHasFoodTargetted=>agentMovesToTargetFood=>agentAtTargetFood",
+    "agentAtTargetFood=>agentPicksUpFood=>agentHasFood",
+    "agentHasFood=>agentEatsFood=>[AGENTHUNGERDECREASED]",
+    "agentKnowsAboutAnimal=>agentTargetsAnimal=>agentHasAnimalTargetted",
+    "agentHasAnimalTargetted=>agentMovesToTargetAnimal=>agentAtTargetAnimal",
+    "agentAtTargetAnimal=>agentKillsTargetAnimal=>agentKnowsAboutFood",
+]
+
+prereqs= {
+    "agentKnowsAboutPlant" : True,
+    "agentHasPlantTargetted" : False,
+    "agentAtTargetPlant" : False,
+    "agentKnowsAboutFood" : False,
+    "agentHasFoodTargetted" : False,
+    "agentAtTargetFood" : False,
+    "agentHasFood" : False,
+    "agentTargetGone" : False,
+    "agentKnowsAboutAnimal" : True,
+    "agentHasAnimalTargetted" : False,
+    "agentAtTargetAnimal" : False,
+}
+
+plans = []
+
+class Node():
+    def __init__(self, goal, parentGoal):
+        self.children = []
+        self.goal = goal
+        self.parentGoal = parentGoal
+
+def BuildPathTree(node):
+
+    for a in actionPaths:
+        action = a.split('=>')
+     
+        if action[-1] == node.goal:
+            goalFound = True
+            myGoal = action[-3]
+            nodes.append(Node(myGoal, node.goal))
+            node.children.append(Node(myGoal, node.goal))
         
+    for n in node.children:
+        BuildPathTree(n)
+
+leafNodes = []
+
+def FindLeafNodes(node):
+    if node.goal in prereqs:
+        if prereqs[node.goal] == True:
+            leafNodes.append(node)
+            return
+
+    for n in node.children:
+        FindLeafNodes(n)
+
+def ClimbTree(node, path, goal):
+    if node.goal == goal:
+        
+        return path
+    for n in nodes:
+        if n.goal == node.parentGoal:
+            path += " => " + node.goal
+            return ClimbTree(n, path, goal)
+
+
+
+goalNode = Node("[AGENTHUNGERDECREASED]", None)
+
+nodes = [goalNode]
+
+BuildPathTree(goalNode)
+
+FindLeafNodes(goalNode)
+
+for n in nodes:
+    print (n.goal)
+
+for l in leafNodes:
+    lpath = ClimbTree(l,"", "[AGENTHUNGERDECREASED]")
+    print(lpath)
+   
 class Action():
     def __init__(self, prereqs, effects, cost, goal):
         self.prereqs = prereqs
